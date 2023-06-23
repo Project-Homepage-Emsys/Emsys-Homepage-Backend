72c5efbf8ee268166cd6d9d9ebe41f50226a2197
LikeDeer <stkjas4657@naver.com>
2023-06-24 01:53:20 +0900

    initialize spring boot project

---
+++ src/main/java/com/emsys/emsyswebsitebackend/EmsysWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emsys.emsyswebsitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsysWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsysWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emsys/emsyswebsitebackend/EmsysWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsys.emsyswebsitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsysWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
