568e990cda6fbbaa95032c0d5c013ad77ce652ac
DDIONG <12yangyang25@naver.com>
2023-06-30 20:29:56 +0900

    Add NoticePost Domain, Repository 생성

---
+++ src/main/java/com/emsys/emsyswebsitebackend/repository/SpringDataJpaNoticePostRepository.java
@@ -0,0 +1,19 @@
+package com.emsys.emsyswebsitebackend.repository;
+
+import com.emsys.emsyswebsitebackend.domain.NoticePost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaNoticePostRepository extends JpaRepository<NoticePost, Long>, NoticePostRepository {
+    @Override
+    Optional<NoticePost> findByUser_Id(Long userId);
+    @Override
+    Optional<NoticePost> findByTitle(String title);
+
+    @Override
+    Optional<NoticePost> findByIsImportant(boolean isImportant);
+
+    @Override
+    Optional<NoticePost> findByCategory(String category);
+}
