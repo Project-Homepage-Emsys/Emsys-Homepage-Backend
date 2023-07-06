ab4846d1a8edda80800652e127654c8b56b61620
LikeNoru <stkjas4657@naver.com>
2023-07-06 21:28:45 +0900

    repository disabled

--- src/main/java/com/emsys/emsyswebsitebackend/repository/NoticeCommentRepository.java
+++ src/main/java/com/emsys/emsyswebsitebackend/repository/NoticeCommentRepository.java
@@ -1,7 +1,7 @@
-package com.emsys.emsyswebsitebackend.repository;
-
-import com.emsys.emsyswebsitebackend.domain.NoticeComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeCommentRepository extends JpaRepository<NoticeComment, String> {
-}
+//package com.emsys.emsyswebsitebackend.repository;
+//
+//import com.emsys.emsyswebsitebackend.domain.NoticeComment;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface NoticeCommentRepository extends JpaRepository<NoticeComment, String> {
+//}
